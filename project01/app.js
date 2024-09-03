060459d01a2c3c738da94ac42040cf143860d143
GarvitDwivedi <garvitdwivedi.29507@gmail.com>
2024-09-04 01:52:59 +0530

    Finally Completed the Project

--- project01/app.js
+++ project01/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const userModel = require('./models/user')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const post = require('./models/post')
+const postModel = require('./models/post')
 const cookieparser = require('cookie-parser')
 
 app.set("view engine" , "ejs" )
@@ -67,9 +67,46 @@ app.get('/logout' , async(req , res)=>{
     res.redirect('/login');
 })
 
+app.post('/post', isLogedin , async(req , res)=>{
+    const user =await userModel.findOne({email: req.user.email});
+    let post = await postModel.create({
+        user : user._id ,
+        content : req.body.content,
+    })
+    user.posts.push(post._id);
+    await user.save() ;
+    res.redirect('/profile')
+})
+
+
+app.get('/like/:id' ,isLogedin ,async(req , res)=>{
+    let post = await postModel.findOne({_id : req.params.id}).populate("user");
+    if(post.likes.indexOf(req.user.userid) === -1){
+        post.likes.push(req.user.userid);
+    }else{
+        post.likes.splice(post.likes.indexOf(req.user.userid) , 1 );
+
+    }
+    await post.save() ;
+    res.redirect('/profile');
+})
+
+
+app.get('/edit/:id' ,isLogedin ,async(req , res)=>{
+    let post = await postModel.findOne({_id : req.params.id}).populate("user");
+    res.render('edit' , {post});
+
+})
+
+app.post('/update/:id' ,isLogedin ,async(req , res)=>{
+    let post = await postModel.findOneAndUpdate({_id : req.params.id} , {content : req.body.content});
+    res.redirect('/profile');
+
+})
+
 
 app.get('/profile' ,isLogedin ,async(req , res)=>{
-    let user = await userModel.findOne({email : req.user.email});
+    let user = await userModel.findOne({email : req.user.email}).populate("posts");
     res.render('profile' ,{user});
 })
 
